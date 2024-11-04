02923b8b828bdbb7dd7a74fd910bbac3b940ad34
KasunJayasanka <officialblackknightcoders@gmail.com>
2024-11-05 00:27:20 +0530

    Project Initiated

---
+++ backend/src/main/java/com/store/velvetbloom/VelvetbloomApplication.java
@@ -0,0 +1,13 @@
+package com.store.velvetbloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelvetbloomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelvetbloomApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/store/velvetbloom/VelvetbloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.velvetbloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelvetbloomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
