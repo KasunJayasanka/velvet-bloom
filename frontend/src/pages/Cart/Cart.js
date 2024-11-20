fd20beb4ac2a11d16f3f0eefd431808015076905
Sasanka Ravindu <61648299+SasankaRW@users.noreply.github.com>
2024-11-20 15:52:34 +0530

    cart logic fixed

--- frontend/src/pages/Cart/Cart.js
+++ frontend/src/pages/Cart/Cart.js
@@ -34,13 +34,15 @@ const Cart = () => {
       <Breadcrumbs title="Cart" />
       {products.length > 0 ? (
         <div className="pb-20">
-          <div className="w-full h-20 bg-[#F5F7F7] text-primeColor hidden lgl:grid grid-cols-5 place-content-center px-6 text-lg font-titleFont font-semibold">
-            <h2 className="col-span-2">Product</h2>
+          <div className="w-full h-20 bg-[#F5F7F7] text-primeColor hidden lgl:grid grid-cols-8 place-content-center px-6 text-lg font-titleFont font-semibold">
+            <h2 className="col-span-3">Product</h2>
+            <h2>Size</h2>
+            <h2>Color</h2>
             <h2>Price</h2>
             <h2>Quantity</h2>
             <h2>Sub Total</h2>
           </div>
-          <div className="mt-5">
+          <div className="mt-6">
             {products.map((item) => (
               <div key={item._id}>
                 <ItemCard item={item} />
@@ -55,19 +57,6 @@ const Cart = () => {
             Reset cart
           </button>
 
-          <div className="flex flex-col mdl:flex-row justify-between border py-4 px-4 items-center gap-2 mdl:gap-0">
-            <div className="flex items-center gap-4">
-              <input
-                className="w-44 mdl:w-52 h-8 px-4 border text-primeColor text-sm outline-none border-gray-400"
-                type="text"
-                placeholder="Coupon Number"
-              />
-              <p className="text-sm mdl:text-base font-semibold">
-                Apply Coupon
-              </p>
-            </div>
-            <p className="text-lg font-semibold">Update Cart</p>
-          </div>
           <div className="max-w-7xl gap-4 flex justify-end mt-4">
             <div className="w-96 flex flex-col gap-4">
               <h1 className="text-2xl font-semibold text-right">Cart totals</h1>
--- frontend/src/redux/velvetSlice.js
+++ frontend/src/redux/velvetSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   userInfo: [],
   products: [], 
-  cart: []  
+  cart:[]
 };
 
 export const velvetSlice = createSlice({
@@ -11,48 +11,40 @@ export const velvetSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      // Find if an exact match exists (including unique ID)
-      const existingItemIndex = state.cart.findIndex(
+      const item = state.products.find(
         (item) => item._id === action.payload._id
       );
-      
-      if (existingItemIndex > -1) {
-        // If exact item exists, increment quantity
-        state.cart[existingItemIndex].quantity += 1;
+      if (item) {
+        item.quantity += action.payload.quantity;
       } else {
-        // If no match, add as new item
-        state.cart.push(action.payload);
+        state.products.push(action.payload);
       }
     },
     increaseQuantity: (state, action) => {
-      // Update to work with cart instead of products
-      const itemIndex = state.cart.findIndex(
+      const item = state.products.find(
         (item) => item._id === action.payload._id
       );
-      if (itemIndex > -1) {
-        state.cart[itemIndex].quantity++;
+      if (item) {
+        item.quantity++;
       }
     },
     decreaseQuantity: (state, action) => {
-
-      // Update to work with cart instead of products
-      const itemIndex = state.cart.findIndex(
+      const item = state.products.find(
         (item) => item._id === action.payload._id
       );
-      if (itemIndex > -1) {
-        if (state.cart[itemIndex].quantity > 1) {
-          state.cart[itemIndex].quantity--;
-        }
+      if (item.quantity === 1) {
+        item.quantity = 1;
+      } else {
+        item.quantity--;
       }
     },
     deleteItem: (state, action) => {
-      // Update to work with cart instead of products
-      state.cart = state.cart.filter(
+      state.products = state.products.filter(
         (item) => item._id !== action.payload
       );
     },
     resetCart: (state) => {
-      state.cart = [];
+      state.products = [];
     },
   },
 });
